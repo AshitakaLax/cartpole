189c4496d19e53dea964346b1fa7a9607c51b4ea
levi Balling <leviballing@gmail.com>
2018-10-20 21:53:50 -0600

    Updated, and implemented the algorthm from the assignment deescription

--- cartpole-sim.py
+++ cartpole-sim.py
@@ -9,7 +9,17 @@ import math
 # constants
 # +- 10 Newtons
 FORCE_MAG = 10.0
-POLEMASS_LENGTH = 0.326
+
+LENGTH = 0.326
+CART_MASS = 0.711
+POLE_MASS = 0.209
+# pole mass * Length of the pole
+POLEMASS_LENGTH = POLE_MASS * LENGTH
+
+TOTAL_MASS = CART_MASS + POLE_MASS
+GRAVITY = 9.82
+FOUR_THIRDS = 4/3
+TAU = 0.02
 
 def cart_pole(action, x, x_dot, theta, theta_dot):
 	"""runs the simulation of the cart pole
@@ -31,6 +41,21 @@ def cart_pole(action, x, x_dot, theta, theta_dot):
 	force = FORCE_MAG if (action > 0)  else -FORCE_MAG
 	cos_theta = math.cos(theta)
 	sin_theta = math.sin(theta)
-	temp = (force + )
+	temp = (force + POLEMASS_LENGTH * theta_dot * theta_dot * sin_theta) / TOTAL_MASS
+	
+	# calculate Theta acceleration
+	theta_acc = (GRAVITY * sin_theta - cos_theta * temp) / (LENGTH * (FOUR_THIRDS - POLE_MASS * cos_theta * cos_theta / TOTAL_MASS))
+
+	x_acc = temp - POLEMASS_LENGTH * theta_acc * cos_theta / TOTAL_MASS
+
+	x = x + (TAU * x_dot)
+	x_dot = x_dot + (TAU * x_acc)
+	theta = theta + (TAU * theta_dot)
+	theta_dot = theta_dot + (TAU * theta_acc)
+
+
+
+
+
 
 

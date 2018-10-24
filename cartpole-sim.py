b1aa1f7e51ef369a00250e4af2e2a1978070362d
levi Balling <leviballing@gmail.com>
2018-10-23 20:32:11 -0600

    still working on shaping the data

--- cartpole-sim.py
+++ cartpole-sim.py
@@ -57,8 +57,10 @@ def cart_pole(action, x, x_dot, theta, theta_dot):
 	x_dot = x_dot + (TAU * x_acc)
 	theta = theta + (TAU * theta_dot)
 	theta_dot = theta_dot + (TAU * theta_acc)
+	return x, x_dot, theta, theta_dot
 
 # Check whether the step should be failed
+# this is deteriming what the reward should be
 def CheckIfFailed(x, x_dot, theta, theta_dot):
 	#calculate the theta Threashold
 	theta_threshold = 12 * 2 * math.pi / 360
@@ -66,30 +68,89 @@ def CheckIfFailed(x, x_dot, theta, theta_dot):
 	return bool(x < -x_threshold or x > x_threshold or theta < -theta_threshold or theta > theta_threshold)
 
 
+gamma = 0.99
+
+def reward_update(rewards):
+	temp_reward = 0
+	result = np.zeros_like(rewards)
+	for i in reversed(range(len(rewards))):
+		result[i] = rewards[i] + gamma * temp_reward
+		temp_reward += rewards[i]
+	return result
+
+
+
+# the shape has to be the format of the 4 parameters after action
+input_net = tflearn.input_data(shape=[None, 4])
+episode_net = tflearn.fully_connected(input_net, 256, activation="relu")
+episode_net = tflearn.fully_connected(episode_net, 256, activation="relu")
+episode_net = tflearn.fully_connected(episode_net, 256, activation="relu")
+result_net = tflearn.fully_connected(episode_net, 2, activation="softmax")
+
+rewards = tf.placeholder(tf.float32, [None])
+actions = tf.placeholder(tf.int32, [None])
+outputs = tf.gather(tf.reshape(result_net, [-1]), tf.range(0, tf.shape(result_net)[0] * tf.shape(result_net)[1], 2) + actions)
+loss = -tf.reduce_mean(tf.log(outputs) * rewards)
+optimizer = tf.train.AdamOptimizer()
+update = optimizer.minimize(loss)
 
 # Setup Episodes to run the simulation
-Episodes = 10
-Max_iterations = 5000
+Episodes = 2000
+Max_iterations = 200
 
 # The 
 s_t = []
+a_t = []
 
 with tf.Session() as episode_session:
+	episode_session.run(tf.global_variables_initializer())
 	for i in range(Episodes):
 		print("Episode: " + str(i))
-		# the shape has to be the format of the 4 parameters after action
-		episode_net = tflearn.input_data(shape=[None, 4])
-		episode_net = tflearn.fully_connected(episode_net, 256, activation="relu")
-		episode_net = tflearn.fully_connected(episode_net, 256, activation="relu")
-		episode_net = tflearn.fully_connected(episode_net, 256, activation="relu")
-		result_net = tflearn.fully_connected(episode_net, 2, activation="softmax")
+		epi_reward = 0
+		epi_history = []
+		cart_pole_params = np.random.uniform(low=-0.05, high=0.05, size=(4,))
+		for j in range(Max_iterations):
+			iteration_result = episode_session.run(result_net, feed_dict={input_net: [cart_pole_params]}).reshape(2)
+			action = np.random.choice(iteration_result, p=iteration_result)
+			action = np.argmax(iteration_result == action)
+			x, x_dot, theta, theta_dot = cart_pole(action, cart_pole_params[0], cart_pole_params[1], cart_pole_params[2], cart_pole_params[3])
+			failed = CheckIfFailed(x, x_dot, theta, theta_dot)
+			reward = 0.0
+			if not failed:
+				reward = 1.0
+			
+			epi_reward += reward
+			epi_history.append([x, x_dot, theta, theta_dot, reward, action])
+			cart_pole_params[0] = x
+			cart_pole_params[1] = x_dot
+			cart_pole_params[2] = theta
+			cart_pole_params[3] = theta_dot
+			
+			# end of the episode
+			if failed:
+				s_t.append(epi_reward)
+				epi_history = np.array(epi_history)
+				epi_history[:, 1] = reward_update(epi_history[:, 1])
+				a_t.extend(epi_history)
+				if i % 10 == 0 and i != 0:
+					a_t = np.array(a_t)
+					episode_session.run(update, feed_dict={input_net: np.vstack(a_t[:, 0]), rewards: a_t[:, 1], actions: a_t[:, 2]})
+					a_t = []
+				break
+		if i % 100 == 0 and i != 0:
+			print(np.mean(s_t[-100:]))
+			if np.mean(s_t[-100:]) == 200:
+				break
 
-		rewards = tf.placeholder(tf.float32, [None])
-		actions = tf.placeholder(tf.int32, [None])
-		outputs = tf.gather(tf.reshape(result_net, [-1]), tf.range(0, tf.shape(result_net)[0] * tf.shape(result_net)[1], 2) + actions)
-		loss = -tf.reduce_mean(tf.log(outputs) * rewards)
-		optimizer = tf.train.AdamOptimizer()
-		update = optimizer.minimize(loss)
+avg_reward = [np.mean(s_t[i-10:i+10]) for i in range(10, len(s_t))]
+print(avg_reward[::10])
+
+
+
+
+
+			
+
 
 
 
